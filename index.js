ea940d43a9175b28333a8bfcdd2af4ea8834486a
Jeffrey Rehm <jrehm@student.neumont.edu>
2021-08-11 22:03:28 -0600

    Added CORS

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 //https://rahmanfadhil.com/express-rest-api/
 
-const express = require("express");
 const mongoose = require("mongoose");
-const routes = require('./routes');
 
 mongoose.connect("mongodb://localhost:25567/trivia_app", {useNewUrlParser: true})
 .then(() => {
+    const express = require("express");
+    const routes = require('./routes');
+    const cors = require('cors');
+
     const app = express();
+    app.use(cors());
+
     app.use(express.json());
     app.use("/api", routes)
     
     app.listen(3000, () => {
         console.log("Server has started")
-    })
+    });
 })
